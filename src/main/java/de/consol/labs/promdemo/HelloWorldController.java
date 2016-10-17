3b9deb8a2c95de63b4ae84c056586d2f6b3bccc5
Fabian Stäber <fabian@fstab.de>
2016-10-17 17:48:01 +0200

    04a-jmx-enabled

---
+++ src/main/java/de/consol/labs/promdemo/HelloWorldController.java
@@ -0,0 +1,43 @@
+package de.consol.labs.promdemo;
+
+import io.prometheus.client.Counter;
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+
+@Controller
+@SpringBootApplication
+@EnablePrometheusEndpoint
+public class HelloWorldController {
+
+    private final Counter promRequestsTotal = Counter.build()
+            .name("requests_total")
+            .help("Total number of requests.")
+            .register();
+    private final CounterService springRequestsTotal;
+
+    public HelloWorldController(@Autowired Collection<PublicMetrics> publicMetrics, @Autowired CounterService sprintRequestsTotal) {
+        this.springRequestsTotal = sprintRequestsTotal;
+        new SpringBootMetricsCollector(publicMetrics).register();
+    }
+
+    @RequestMapping(path = "/hello-world")
+    public @ResponseBody String sayHello() {
+        promRequestsTotal.inc();
+        springRequestsTotal.increment("counter.calls.promdemo.hello_world");
+        return "hello, world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
