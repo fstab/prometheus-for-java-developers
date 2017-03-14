df99cfc0ce9e531a5c2e61bf0946aed0ac51cff0
Fabian Stäber <fabian@fstab.de>
2017-03-14 17:02:03 +0100

    05a-dropwizard-enabled

--- src/main/java/de/consol/labs/promdemo/HelloWorldController.java
+++ src/main/java/de/consol/labs/promdemo/HelloWorldController.java
@@ -1,6 +1,10 @@
 package de.consol.labs.promdemo;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.common.TextFormat;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +18,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
@@ -29,10 +36,14 @@ public class HelloWorldController {
             .register();
     private final CounterService springRequestsTotal;
     private final AtomicInteger jmxRequestsTotal = new AtomicInteger();
+    private final com.codahale.metrics.Counter dwRequestsTotal;
 
     public HelloWorldController(@Autowired Collection<PublicMetrics> publicMetrics, @Autowired CounterService sprintRequestsTotal) {
         this.springRequestsTotal = sprintRequestsTotal;
         new SpringBootMetricsCollector(publicMetrics).register();
+        MetricRegistry dropwizardRegistry = new MetricRegistry();
+        dwRequestsTotal = dropwizardRegistry.counter("dropwizard_requests_total");
+        ConsoleReporter.forRegistry(dropwizardRegistry).build().start(5, TimeUnit.SECONDS);
     }
 
     @RequestMapping(path = "/hello-world")
@@ -40,6 +51,7 @@ public class HelloWorldController {
         promRequestsTotal.inc();
         springRequestsTotal.increment("counter.calls.promdemo.hello_world");
         jmxRequestsTotal.incrementAndGet();
+        dwRequestsTotal.inc();
         return "hello, world";
     }
 
