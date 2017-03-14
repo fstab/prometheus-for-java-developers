c9a3049aa4353389bbac5fe591e01d2b34e56ace
Fabian Stäber <fabian@fstab.de>
2017-03-14 16:59:19 +0100

    03b-spring-boot-actuator-custom-metric

--- src/main/java/de/consol/labs/promdemo/HelloWorldController.java
+++ src/main/java/de/consol/labs/promdemo/HelloWorldController.java
@@ -2,7 +2,9 @@ package de.consol.labs.promdemo;
 
 import io.prometheus.client.Counter;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,16 @@ public class HelloWorldController {
             .name("requests_total")
             .help("Total number of requests.")
             .register();
+    private final CounterService springRequestsTotal;
+
+    public HelloWorldController(@Autowired CounterService sprintRequestsTotal) {
+        this.springRequestsTotal = sprintRequestsTotal;
+    }
 
     @RequestMapping(path = "/hello-world")
     public @ResponseBody String sayHello() {
         promRequestsTotal.inc();
+        springRequestsTotal.increment("counter.calls.promdemo.hello_world");
         return "hello, world";
     }
 
