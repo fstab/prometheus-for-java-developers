9c2b1a7a4de990daf859cc41efa8363764229dbc
Fabian Stäber <fabian@fstab.de>
2017-03-14 17:00:43 +0100

    04b-jmx-custom-metric

--- src/main/java/de/consol/labs/promdemo/HelloWorldController.java
+++ src/main/java/de/consol/labs/promdemo/HelloWorldController.java
@@ -8,14 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.endpoint.PublicMetrics;
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 @SpringBootApplication
+@ManagedResource(objectName="de.consol.labs:name=PromDemoBean")
 @EnablePrometheusEndpoint
 public class HelloWorldController {
 
@@ -24,6 +28,7 @@ public class HelloWorldController {
             .help("Total number of requests.")
             .register();
     private final CounterService springRequestsTotal;
+    private final AtomicInteger jmxRequestsTotal = new AtomicInteger();
 
     public HelloWorldController(@Autowired Collection<PublicMetrics> publicMetrics, @Autowired CounterService sprintRequestsTotal) {
         this.springRequestsTotal = sprintRequestsTotal;
@@ -34,7 +39,13 @@ public class HelloWorldController {
     public @ResponseBody String sayHello() {
         promRequestsTotal.inc();
         springRequestsTotal.increment("counter.calls.promdemo.hello_world");
+        jmxRequestsTotal.incrementAndGet();
         return "hello, world";
+    }
+
+    @ManagedAttribute(description="jmx_requests_total")
+    public int getJmxRequestsTotal() {
+        return jmxRequestsTotal.get();
     }
 
     public static void main(String[] args) {
